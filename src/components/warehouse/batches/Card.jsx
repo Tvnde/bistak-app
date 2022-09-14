4122109d58e070d656dbfbc386a7f416856b5b34
vreemdeling <bamgbade_tunde@yahoo.com>
2022-09-14 19:23:17 +0100

    front-end changes

--- src/components/warehouse/batches/Card.jsx
+++ src/components/warehouse/batches/Card.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux"
 import { useNavigate } from "react-router";
 import { getProducts } from '../../../actions/products'
 
-import { DriveFolderUploadOutlined } from '@mui/icons-material'
+import { DriveFolderUploadOutlined, RemoveRedEye } from '@mui/icons-material'
 import { Inventory2 } from '@mui/icons-material'
 
 import './card.scss'
@@ -23,7 +23,7 @@ const Card = ({batch}) => {
         <div className="card-content">
           <div className="card-above">
             <div className="product-img">
-              <img src= {product && product.image!="" ? products.filter((product) => {return product._id == batch.product})[0].image : "https://bistakstore.s3.amazonaws.com/images/Bistak-Grocery-Logo_2.png"} alt="" />
+              <img src= {(product && product.image && product.image!="") ? products.filter((product) => {return product._id == batch.product})[0].image : "https://bistakstore.s3.amazonaws.com/images/Bistak-Grocery-Logo_2.png"} alt="" />
             </div>
             <div className="product-name">
             {products.filter((product) => {return product._id == batch.product})[0] ? products.filter((product) => {return product._id == batch.product})[0].name : null}
@@ -36,6 +36,9 @@ const Card = ({batch}) => {
             <div className="card-below-bottom">
               <div className="card-detail"><div className="card-detail-text">{batch.collection_count}</div><div className="card-detail-title">ITEMS</div></div>
               <div className="card-detail"><div className="card-detail-text">{batch.date_created.split("T")[0]}</div><div className="card-detail-title">DATE</div></div>
+            </div>
+            <div className="card-below-links">
+              <div className="card-link"><span><RemoveRedEye style={{fontSize: '18px'}}/></span> View Details</div>
             </div>
           </div>
         </div>
